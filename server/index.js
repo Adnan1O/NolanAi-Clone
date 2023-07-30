e962088d2d14d928335d13509567f1038d5bead2
Adnan1O <134485508+Adnan1O@users.noreply.github.com>
2023-07-30 14:41:59 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json())
 app.use(cors())
 
-const API_KEY = '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'
+const API_KEY = 'your_key'
 
 app.post('/sendscript', async(req,res)=>{
     const {title, plot, genre} = req.body;
